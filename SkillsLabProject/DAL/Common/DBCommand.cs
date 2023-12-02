ab20a14c2519ea1a2c17f6b52f7afa01bc943906
Ved Rowjee <ved.rowjee@ceridian.com>
2023-12-02 13:19:40 +0400

    refactor

--- SkillsLabProject/DAL/Common/DBCommand.cs
+++ SkillsLabProject/DAL/Common/DBCommand.cs
@@ -13,7 +13,6 @@ namespace SkillsLabProject.DAL.Common
         {
             DAL dal = new DAL();
             DataTable dataTable = new DataTable();
-
             using (SqlCommand command = new SqlCommand(query, dal.Connection))
             {
                 command.CommandType = CommandType.Text;
@@ -22,47 +21,13 @@ namespace SkillsLabProject.DAL.Common
                     dataAdapter.Fill(dataTable);
                 }
             }
-
             dal.CloseConnection();
-
             return dataTable;
         }
-
-        public static bool InsertUpdateData(string query, List<SqlParameter> parameters)
-        {
-            try
-            {
-                DAL dal = new DAL();
-                int rowsAffected = 0;
-
-                using (SqlCommand command = new SqlCommand(query, dal.Connection))
-                {
-                    command.CommandType = CommandType.Text;
-                    if (parameters != null)
-                    {
-                        parameters.ForEach(parameter =>
-                        {
-                            command.Parameters.AddWithValue(parameter.ParameterName, parameter.Value);
-                        });
-                    }
-                    rowsAffected = command.ExecuteNonQuery();
-                }
-                dal.CloseConnection();
-
-                return rowsAffected > 0;
-            }
-            catch
-            {
-                return false;
-            }
-
-        }
-
         public static DataTable GetDataWithCondition(string query, List<SqlParameter> parameters)
         {
             DAL dal = new DAL();
             DataTable dt = new DataTable();
-
             using (SqlCommand command = new SqlCommand(query, dal.Connection))
             {
                 command.CommandType = CommandType.Text;
@@ -77,21 +42,43 @@ namespace SkillsLabProject.DAL.Common
                 {
                     sda.Fill(dt);
                 }
-
             }
-
             dal.CloseConnection();
-
             return dt;
         }
-
+        public static bool InsertUpdateData(string query, List<SqlParameter> parameters)
+        {
+            try
+            {
+                DAL dal = new DAL();
+                int rowsAffected = 0;
+                using (SqlCommand command = new SqlCommand(query, dal.Connection))
+                {
+                    command.CommandType = CommandType.Text;
+                    if (parameters != null)
+                    {
+                        parameters.ForEach(parameter =>
+                        {
+                            command.Parameters.AddWithValue(parameter.ParameterName, parameter.Value);
+                        });
+                    }
+                    rowsAffected = command.ExecuteNonQuery();
+                }
+                dal.CloseConnection();
+                return rowsAffected > 0;
+            }
+            catch (Exception)
+            {
+                return false;
+                throw;
+            }
+        }
         public static bool DeleteData(string query, SqlParameter parameter)
         {
             try
             {
                 DAL dal = new DAL();
                 int rowsAffected = 0;
-
                 using (SqlCommand command = new SqlCommand(query, dal.Connection))
                 {
                     command.CommandType = CommandType.Text;
@@ -107,8 +94,8 @@ namespace SkillsLabProject.DAL.Common
             catch
             {
                 return false;
+                throw;
             }
         }
-
     }
 }
--- SkillsLabProject/DAL/EmployeeDAL.cs
+++ SkillsLabProject/DAL/EmployeeDAL.cs
@@ -17,75 +17,58 @@ namespace SkillsLabProject.DAL
     {
         IEnumerable<EmployeeModel> GetAllEmployees();
         EmployeeModel GetEmployee(LoginViewModel model);
-        bool UpdateEmployee(EmployeeModel model);
+        bool UpdateEmployee(EmployeeModel employee);
         bool DeleteEmployee(int employeeId);
     }
     public class EmployeeDAL : IEmployeeDAL
     {
-        private const string GetEmployeeQuery = @"
-            SELECT e.EmployeeId, e.FirstName, e.LastName, e.NIC, e.PhoneNumber, e.DepartmentId, e.RoleId, a.Email, d.Title
-            FROM [dbo].[Employee] as e
-            INNER JOIN [dbo].[AppUser] as a ON e.EmployeeId = a.EmployeeId
-            INNER JOIN [dbo].[Department] as d ON e.DepartmentId = d.DepartmentId
-            WHERE a.[Email] = @Email
-        ";
-        private const string GetAllEmployeesQuery = @"
-            SELECT e.EmployeeId, FirstName, LastName, NIC, PhoneNumber, e.DepartmentId, RoleId, a.Email, d.Title
-            FROM [dbo].[Employee] as e
-            INNER JOIN [dbo].[AppUser] as a ON e.EmployeeId = a.EmployeeId
-            INNER JOIN [dbo].[Department] as d ON e.DepartmentId = d.DepartmentId
-        ";
-        private const string UpdateEmployeeQuery = @"
-            UPDATE [dbo].[Employee] e
-            INNER JOIN [dbo].[AppUser] as a ON e.EmployeeId = a.EmployeeId
-            INNER JOIN [dbo].[Department] as d ON e.DepartmentId = d.DepartmentId
-            SET FirstName=@FirstName, LastName=@LastName, NIC=@NIC, PhoneNumber=@PhoneNumber, e.DepartmentId=@DepartmentId, RoleId=@RoleId,  a.Email=@Email, d.Title=@Title
-            WHERE EmployeeId=@EmployeeId;
-        ";
-        private const string DeleteEmployeeQuery = @"
-            DELETE FROM AppUser WHERE EmployeeId=@EmployeeId;
-            DELETE FROM Enrollement WHERE EmployeeId=@EmployeeId;
-            DELETE FROM Employee WHERE EmployeeId=@EmployeeId
-        ";
-
-
         public IEnumerable<EmployeeModel> GetAllEmployees()
         {
+            const string GetAllEmployeesQuery = @"
+                SELECT e.EmployeeId, FirstName, LastName, NIC, PhoneNumber, e.DepartmentId, RoleId, a.Email, d.Title
+                FROM [dbo].[Employee] as e
+                INNER JOIN [dbo].[AppUser] as a ON e.EmployeeId = a.EmployeeId
+                INNER JOIN [dbo].[Department] as d ON e.DepartmentId = d.DepartmentId
+            ";
+            var dt = DBCommand.GetData(GetAllEmployeesQuery);
             var employees = new List<EmployeeModel>();
             EmployeeModel employee;
-
-            var dt = DBCommand.GetData(GetAllEmployeesQuery);
             foreach (DataRow row in dt.Rows)
             {
-                employee = new EmployeeModel();
-                employee.EmployeeId = int.Parse(row["EmployeeId"].ToString());
-                employee.FirstName = row["FirstName"].ToString();
-                employee.LastName = row["LastName"].ToString();
-                employee.NIC = row["NIC"].ToString();
-                employee.PhoneNumber = row["PhoneNumber"].ToString();
-
-                employee.Email = row["Email"].ToString();
-
-                var department = new DepartmentModel();
-                department.DepartmentId = int.Parse(row["DepartmentId"].ToString());
-                department.Title = row["Title"].ToString();
-                employee.Department = department;
-
-                employee.Role = (Role) int.Parse(row["RoleId"].ToString());
-
+                employee = new EmployeeModel
+                {
+                    EmployeeId = int.Parse(row["EmployeeId"].ToString()),
+                    FirstName = row["FirstName"].ToString(),
+                    LastName = row["LastName"].ToString(),
+                    NIC = row["NIC"].ToString(),
+                    PhoneNumber = row["PhoneNumber"].ToString(),
+                    Email = row["Email"].ToString(),
+                    Department = new DepartmentModel
+                    {
+                        DepartmentId = int.Parse(row["DepartmentId"].ToString()),
+                        Title = row["Title"].ToString()
+                    },
+                    Role = (Role)int.Parse(row["RoleId"].ToString())
+                };
                 employees.Add(employee);
             }
-
             return employees;
         }
-
-        public EmployeeModel GetEmployee(LoginViewModel model)
+        public EmployeeModel GetEmployee(LoginViewModel login)
         {
-            var employee = new EmployeeModel();
-            var parameters = new List<SqlParameter>();
-            parameters.Add(new SqlParameter("@Email", model.Email));
-
+            const string GetEmployeeQuery = @"
+                SELECT e.EmployeeId, e.FirstName, e.LastName, e.NIC, e.PhoneNumber, e.DepartmentId, e.RoleId, a.Email, d.Title
+                FROM [dbo].[Employee] as e
+                INNER JOIN [dbo].[AppUser] as a ON e.EmployeeId = a.EmployeeId
+                INNER JOIN [dbo].[Department] as d ON e.DepartmentId = d.DepartmentId
+                WHERE a.[Email] = @Email
+            ";
+            var parameters = new List<SqlParameter>
+            {
+                new SqlParameter("@Email", login.Email)
+            };
             var dt = DBCommand.GetDataWithCondition(GetEmployeeQuery, parameters);
+            var employee = new EmployeeModel();
             foreach (DataRow row in dt.Rows)
             {
                 employee.EmployeeId = int.Parse(row["EmployeeId"].ToString());
@@ -93,40 +76,46 @@ namespace SkillsLabProject.DAL
                 employee.LastName = row["LastName"].ToString();
                 employee.NIC = row["NIC"].ToString();
                 employee.PhoneNumber = row["PhoneNumber"].ToString();
-
                 employee.Email = row["Email"].ToString();
-
-                var department = new DepartmentModel();
-                department.DepartmentId = int.Parse(row["DepartmentId"].ToString());
-                department.Title = row["Title"].ToString();
-                employee.Department = department;
-
+                employee.Department = new DepartmentModel
+                {
+                    DepartmentId = int.Parse(row["DepartmentId"].ToString()),
+                    Title = row["Title"].ToString()
+                };
                 employee.Role = (Role)int.Parse(row["RoleId"].ToString());
             }
-
             return employee;
         }
-
-        public bool UpdateEmployee(EmployeeModel model)
+        public bool UpdateEmployee(EmployeeModel employee)
         {
-            var parameters = new List<SqlParameter>();
-            parameters.Add(new SqlParameter("@EmployeeId", model.EmployeeId));
-            parameters.Add(new SqlParameter("@FirstName", model.FirstName));
-            parameters.Add(new SqlParameter("@LastName", model.LastName));
-            parameters.Add(new SqlParameter("@NIC", model.NIC));
-            parameters.Add(new SqlParameter("@Email", model.Email));
-            parameters.Add(new SqlParameter("@PhoneNumber", model.PhoneNumber));
-            parameters.Add(new SqlParameter("@DepartmentId", model.Department.DepartmentId));
-            parameters.Add(new SqlParameter("@Title", model.Department.Title));
-            parameters.Add(new SqlParameter("@RoleId", (int) model.Role));
-
-            var isValid = DBCommand.InsertUpdateData(UpdateEmployeeQuery, parameters);
-
-            return isValid;
+            const string UpdateEmployeeQuery = @"
+                UPDATE [dbo].[Employee] e
+                INNER JOIN [dbo].[AppUser] as a ON e.EmployeeId = a.EmployeeId
+                INNER JOIN [dbo].[Department] as d ON e.DepartmentId = d.DepartmentId
+                SET FirstName=@FirstName, LastName=@LastName, NIC=@NIC, PhoneNumber=@PhoneNumber, e.DepartmentId=@DepartmentId, RoleId=@RoleId,  a.Email=@Email, d.Title=@Title
+                WHERE EmployeeId=@EmployeeId;
+            ";
+            var parameters = new List<SqlParameter>
+            {
+                new SqlParameter("@EmployeeId", employee.EmployeeId),
+                new SqlParameter("@FirstName", employee.FirstName),
+                new SqlParameter("@LastName", employee.LastName),
+                new SqlParameter("@NIC", employee.NIC),
+                new SqlParameter("@Email", employee.Email),
+                new SqlParameter("@PhoneNumber", employee.PhoneNumber),
+                new SqlParameter("@DepartmentId", employee.Department.DepartmentId),
+                new SqlParameter("@Title", employee.Department.Title),
+                new SqlParameter("@RoleId", (int)employee.Role)
+            };
+            return DBCommand.InsertUpdateData(UpdateEmployeeQuery, parameters);
         }
-
         public bool DeleteEmployee(int employeeId)
         {
+            const string DeleteEmployeeQuery = @"
+                DELETE FROM AppUser WHERE EmployeeId=@EmployeeId;
+                DELETE FROM Enrollement WHERE EmployeeId=@EmployeeId;
+                DELETE FROM Employee WHERE EmployeeId=@EmployeeId
+            ";
             var parameter = new SqlParameter("@EmployeeId", employeeId);
             return DBCommand.DeleteData(DeleteEmployeeQuery, parameter);
         }
