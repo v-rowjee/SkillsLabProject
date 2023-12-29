5719a96cdd4a30c80f70e2b6e9b96cdbffddf071
Ved Rowjee <ved.rowjee@ceridian.com>
2023-12-29 15:28:27 +0400

    dbcommand

--- SkillsLab.Common/DAL/DBCommand.cs
+++ SkillsLab.Common/DAL/DBCommand.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.SqlClient;
 using System.Data;
-using System.Security.Cryptography;
+using System.Data.SqlClient;
 
 namespace SkillsLabProject.Common.DAL
 {
