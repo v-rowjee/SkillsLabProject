34f42c8c12f37cde5fa061e9564f7c6f682838c5
Ved Rowjee <ved.rowjee@ceridian.com>
2024-01-04 09:34:12 +0400

    logger

---
+++ SkillsLab.Common/Log/Logger.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace SkillsLabProject.Common.Log
+{
+    public class Logger : ILogger
+    {
+        private readonly string logFilePath;
+
+        public Logger(string filePath = "App_Data\\ErrorLog.txt")
+        {
+            string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            logFilePath = Path.Combine(baseDirectory, filePath);
+        }
+
+        public void Log(string message)
+        {
+            string logMessage = $"{message}";
+
+            try
+            {
+                if (!File.Exists(logFilePath))
+                {
+                    File.Create(logFilePath);
+                }
+
+                using (StreamWriter writer = File.AppendText(logFilePath))
+                {
+                    writer.WriteLine(logMessage);
+                }
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine($"Error writing to log file: {error.Message}");
+                throw;
+            }
+        }
+
+    }
+}
