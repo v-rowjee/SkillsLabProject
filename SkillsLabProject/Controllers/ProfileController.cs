22cad5d83911cfebd056905f4cd3c48f27cdd1c2
Ved Rowjee <ved.rowjee@ceridian.com>
2024-01-13 09:19:26 +0400

    Dojo profile

--- SkillsLabProject/Controllers/ProfileController.cs
+++ SkillsLabProject/Controllers/ProfileController.cs
@@ -1,16 +1,38 @@
-﻿using System;
+﻿using SkillsLabProject.BL.BL;
+using SkillsLabProject.Common.Models.ViewModels;
+using SkillsLabProject.Custom;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
 namespace SkillsLabProject.Controllers
 {
+    [UserSession]
     public class ProfileController : Controller
     {
-        // GET: Dojo
-        public ActionResult Index()
+        private readonly IEmployeeBL _employeeBL;
+        private readonly INotificationBL _notificationBL;
+
+        public ProfileController(IEmployeeBL employeeBL, INotificationBL notificationBL)
         {
+            _employeeBL = employeeBL;
+            _notificationBL = notificationBL;
+        }
+
+        // GET: Dojo
+        [HttpGet]
+        [CustomAuthorization("Employee,Manager,Admin")]
+        public async Task<ActionResult> Index()
+        {
+            var loggeduser = Session["CurrentUser"] as LoginViewModel;
+            var employee = await _employeeBL.GetEmployeeAsync(loggeduser);
+            ViewBag.Employee = employee;
+
+            var notificationCount = await _notificationBL.GetNotificationCountAsync(employee);
+            ViewBag.NotificationCount = notificationCount;
             return View();
         }
     }
